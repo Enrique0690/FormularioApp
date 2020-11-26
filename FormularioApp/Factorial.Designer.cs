400a3a1fac395d5e98410a9c9727707513a3586b
Enrique <enrique.reyesmarin@gmail.com>
2020-11-25 19:27:20 -0500

    Factorial

---
+++ FormularioApp/Factorial.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FormularioApp
+{
+    partial class Factorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRespuesta = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.AutoSize = true;
+            this.txtRespuesta.BackColor = System.Drawing.Color.Transparent;
+            this.txtRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRespuesta.ForeColor = System.Drawing.SystemColors.Control;
+            this.txtRespuesta.Location = new System.Drawing.Point(157, 153);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(0, 18);
+            this.txtRespuesta.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(16, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "RESPUESTA: ";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(21, 87);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(348, 31);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(113, 20);
+            this.txtNumero.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(17, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "INGRESE EL NUMERO A CALCULAR: ";
+            // 
+            // Factorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FormularioApp.Properties.Resources._2;
+            this.ClientSize = new System.Drawing.Size(493, 245);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "Factorial";
+            this.Text = "Factorial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtRespuesta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FormularioApp/Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioApp
+{
+    public partial class Factorial : Form
+    {
+        public Factorial()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int num, i;
+            double result = 1;
+
+            num = int.Parse(txtNumero.Text);
+            for (i = 2; i <= num; i++)
+            {
+                result = result * i;
+            }
+
+            txtRespuesta.Text = "El factorial del numero es :  " + result;
+        }
+    }
+}
